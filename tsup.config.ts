fe21b79bee211025eb8884e28946d1edb2461a16
Edoardo Ranghieri <edoardo@ranghieri.com>
2024-01-09 19:52:02 +0100

    fix(tsup): disable bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/server.ts", "src/client.tsx", "src/types.ts"],
+	entry: ["src/server.ts", "src/client.tsx", "src/types.ts", "src/action.ts"],
 	format: ["esm"],
 	clean: true,
 	splitting: false,
 	sourcemap: true,
 	dts: true,
+	bundle: false,
 });
